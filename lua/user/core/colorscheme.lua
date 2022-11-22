78fde389515d7d8114e0526437742c1325840036
Peter Merikan <peter@merikan.com>
2022-11-22 13:56:35 +0100

    polish

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -1,6 +1,6 @@
--- set colorscheme to use
+-- colorscheme to use
 local colorscheme = "gruvbox-material"
--- load color schemes custom settings if available
+-- load custom settings for color schemes if available
 ok, scheme = pcall(require, "user.colorschemes." .. colorscheme)
 if ok then
   scheme.setup()
