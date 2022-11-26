bdef487161a987e5352f3f67164a9df715e6721d
Peter Merikan <peter@merikan.com>
2022-11-26 19:58:18 +0100

    refactor packer

--- lua/user/plugins/packer.lua
+++ lua/user/plugins/packer.lua
@@ -1,33 +1,28 @@
-local fn = vim.fn
+-- install packer.nvim and plugins
+-- https://github.com/wbthomason/packer.nvim
+--
+-- :PackerCompile                    - Regenerate compiled loader file
+-- :PackerClean                      - Remove any disabled or unused plugins
+-- :PackerInstall                    - Clean, then install missing plugins
+-- :PackerUpdate                     - Clean, then update and install plugins
+-- :PackerSync                       - Perform `PackerUpdate` and then `PackerCompile`
+-- :PackerLoad completion-nvim ale   - Loads opt plugin immediately
 
--- Automatically install packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local CURDIR = (...):match "(.-)[^%.]+$"
+
+local packer_init = false
+local packer_init = false
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+  packer_init = true
+  vim.api.nvim_echo({ { "Installing packer.nvim", "Type" } }, true, {})
+  execute("!git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. install_path)
+  execute "packadd packer.nvim"
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
+local packer = require "packer"
 
 -- Have packer use a popup window
 packer.init {
@@ -36,10 +31,17 @@ packer.init {
       return require("packer.util").float { border = "rounded" }
     end,
   },
-  git = {
-    clone_timeout = 300, -- Timeout, in seconds, for git clones
-  },
 }
+
+-- reload if this buffer changes
+vim.cmd [[
+  augroup packer_refresh
+  autocmd!
+  autocmd BufWritePost plugins.lua PackerClean
+  autocmd BufWritePost plugins.lua PackerInstall
+  autocmd BufWritePost plugins.lua PackerCompile
+  augroup END
+]]
 
 -- Install your plugins here
 return packer.startup(function(use)
@@ -119,7 +121,7 @@ return packer.startup(function(use)
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
+  if packer_init then
     require("packer").sync()
   end
 end)
